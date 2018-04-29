1f6baa2b00b1b9a4452e70de82b7b44f31243cc5
SlandShow <SlandShow12345@mail.ru>
2018-04-29 14:18:35 +0300

    DOM html update

--- script.js
+++ script.js
@@ -12,7 +12,10 @@ VK.Api.call('wall.get', {domain: 'homelike_peace', count: 3, v:"5.73"}, function
 		}
 
 	// Text
-	var text = res.response.items[0].text;
+	var text = res.response.items[0].text + "\nКоличесто лайков: " + 
+			res.response.items[0].likes.count.toString() + "\nКоличество просмотров: " +
+			res.response.items[0].views.count.toString() +  "\nКоличество репостов: " +
+			res.response.items[0].reposts.count.toString();
 	console.log(text);
 	document.getElementById("view").innerHTML = text;
 });
