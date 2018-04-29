f1a319ed0eb272a46fb0dd65b95c1573a60881e7
SlandShow <SlandShow12345@mail.ru>
2018-04-29 14:06:30 +0300

    VK API parsing

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 VK.init({
     apiId: 	6463415
   });
+
+
+
+VK.Api.call('wall.get', {domain: 'homelike_peace', count: 3, v:"5.73"}, function(res) {
+
+	if(!res.response) {
+			console.error("Информация о группах не была получена");
+			return false;
+		}
+
+	// Text
+	var text = res.response.items[0].text;
+	console.log(text);
+	document.getElementById("view").innerHTML = text;
+});
